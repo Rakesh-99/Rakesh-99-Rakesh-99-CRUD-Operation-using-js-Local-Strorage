349357998df45d24eb845498031ef172090134c0
RakeshKumarParida <rakeshkumarparida424@gmail.com>
2022-05-11 01:59:38 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Rakesh-99-CRUD-Operation-using-js-Local-Strorage
+This App is under construction
+
+To view click on https://rakesh-99.github.io/Rakesh-99-CRUD-Operation-using-js-Local-Strorage/
---
+++ script.js
@@ -0,0 +1,65 @@
+let input = document.getElementById("input");
+let errormsg = document.getElementById("error");
+const formSubmit = document.getElementById("form");
+let posts = document.getElementById("posts");
+
+
+
+
+
+formSubmit.addEventListener("submit", (obj) => {
+    obj.preventDefault();
+    formValidate();
+});
+
+
+
+
+
+//Function for validating the form if it is blanck or not 
+
+let formValidate = () => {
+    if (input.value === "") {
+        errormsg.innerHTML = "Input filed is blancked ! *"
+        console.log("failure state");
+    } else {
+      errormsg.innerHTML = "";
+      console.log("success state");
+      getInput(); //calling getInput method. It will fire when user clicks on submit
+      displayData(); //calling displayData method. It will fire when user clicks on submit
+    }
+};
+
+// Function for collecting the input 
+
+let collect = {};
+
+let getInput = () => {
+    collect["text"] = input.value;
+    console.log(collect);
+};
+
+
+
+
+
+//Function for displayimg the data
+
+let displayData = () => {
+    posts.innerHTML += 
+        `
+      <div>
+        <p>${collect.text}</p>
+        <span class="option">
+          <i class="bi bi-pencil-square"></i>
+          <i class="bi bi-trash3"></i>
+        </span>
+      </div>
+    `
+    input.value = ""; //Resseting the form after posting stuffs
+}
+
+
+
+
+
